57823ea0a6ec35b8c3fdac7280b5934007c2cb59
jangyeeunee <amy5132@naver.com>
2024-11-17 11:49:54 +0900

    로그인 설정

--- Twitter/src/buttonAction.java
+++ Twitter/src/buttonAction.java
@@ -1,15 +1,20 @@
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class buttonAction implements ActionListener {
-
     private String actionType;
+    private String id;
+    private String password;
 
-    public buttonAction(String getActionType) {
-
-        this.actionType = getActionType;
+    public buttonAction(String actionType, String id, String password) {
+        this.actionType = actionType;
+        this.id = id;
+        this.password = password;
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
-        dbConnect connect = new dbConnect(actionType);
+        dbConnect db = new dbConnect(actionType, id, password);
+        db.getActionDb();
     }
 }
