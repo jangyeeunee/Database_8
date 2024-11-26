ed871808ad9c2d3ff81223e5912e704770d746e3
inyong04 <145008900+inyong04@users.noreply.github.com>
2024-11-26 21:07:18 +0900

    Following/er 추가

--- Twitter/src/UserInfo.java
+++ Twitter/src/UserInfo.java
@@ -1,8 +1,6 @@
-//get 추가함
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 public class UserInfo {
     private static UserInfo instance;
 
@@ -12,13 +10,16 @@ public class UserInfo {
     private String userEmail;
     private String userPassword;
     private String userPhone;
-    private String userAddress;
     private String userBirth;
     private String userGender;
+    private int followerCount;
+    private int followingCount;
 
+    // Private constructor for Singleton pattern
     private UserInfo() {
     }
 
+    // Get the single instance of UserInfo
     public static UserInfo getInstance() {
         if (instance == null) {
             instance = new UserInfo();
@@ -26,55 +27,88 @@ public class UserInfo {
         return instance;
     }
 
-    public void setUserInfo(ResultSet rs) {
-        try {
-            this.userId = rs.getString("id");
-            this.userPassword = rs.getString("pwd");
-            this.userFirstName = rs.getString("first_Name");
-            this.userLastName = rs.getString("last_name");
-            this.userEmail = rs.getString("email");
-            this.userPhone = rs.getString("phone_number");
-            this.userBirth = rs.getString("birth");
-            this.userGender = rs.getString("gender");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Getter methods for each field
+    // Getters and Setters for user properties
     public String getUserId() {
         return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getUserFirstName() {
         return userFirstName;
     }
 
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
     public String getUserLastName() {
         return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
     }
 
     public String getUserEmail() {
         return userEmail;
     }
 
-    public String getUserPassword() {
-        return userPassword;
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
     public String getUserPhone() {
         return userPhone;
     }
 
-    public String getUserAddress() {
-        return userAddress;
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
     }
 
     public String getUserBirth() {
         return userBirth;
     }
 
+    public void setUserBirth(String userBirth) {
+        this.userBirth = userBirth;
+    }
+
     public String getUserGender() {
         return userGender;
+    }
+
+    public void setUserGender(String userGender) {
+        this.userGender = userGender;
+    }
+
+    
+    public int getFollowerCount() {
+        return followerCount;
+    }
+
+    public void setFollowerCount(int followerCount) {
+        this.followerCount = followerCount;
+    }
+
+    public int getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(int followingCount) {
+        this.followingCount = followingCount;
+    }
+
+    // Method to set all user information from a ResultSet (after login or query)
+    public void setUserInfo(ResultSet rs) throws SQLException {
+        this.userId = rs.getString("id");
+        this.userFirstName = rs.getString("first_name");
+        this.userLastName = rs.getString("last_name");
+        this.userEmail = rs.getString("email");
+        this.userPhone = rs.getString("phone_number");
+        this.userBirth = rs.getString("birth");
+        this.userGender = rs.getString("gender");
     }
 }
